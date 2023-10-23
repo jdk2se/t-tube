608daa73678b3ced08f6fb89cdd90456d9c286f9
Evgenii Dudnik <johnydvgu@gmail.com>
2023-10-23 17:03:14 +0300

    Finish categories list

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,14 @@ export default function App() {
     <PageHeader />
     <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
       <div>Sidebar</div>
-      <div className="sticky top-0 bg-white z-10 pb-4">
-        <Categories 
-          categories={categories} 
-          selectedCategory={selectedCategory} 
-          onSelect={setSelectedCategory}
-        />
+      <div className="overflox-x-hidden px-8 pb-4">
+        <div className="sticky top-0 bg-white z-10 pb-4">
+          <Categories 
+            categories={categories} 
+            selectedCategory={selectedCategory} 
+            onSelect={setSelectedCategory}
+          />
+        </div>
       </div>
     </div>
   </div>
